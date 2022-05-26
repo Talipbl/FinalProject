a46daac32756c89251897f3cf63552389ec4201d
Talip BULUNDU <talipbulundu@gmail.com>
2022-05-26 03:20:57 +0300

    Code refactor

--- Business/Abstracts/IProductService.cs
+++ Business/Abstracts/IProductService.cs
@@ -13,7 +13,6 @@ namespace Business.Abstract
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int categoryId);
         List<Product> GetByUnitPrice(decimal min, decimal max);
-
         List<ProductDetailDTO> GetProductDetails();
         void Add(Product product);
         void Delete(Product product);
